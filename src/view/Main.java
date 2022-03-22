94a1ed389cabed652f16cb4d1585c9eff2a3903d
Leo <Leo@DESKTOP-K9P38CG>
2022-03-22 15:08:44 -0300

    Primeiro Commit

---
+++ src/view/Main.java
@@ -0,0 +1,11 @@
+package view;
+
+import controller.CozinhaThread;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i = 0; i < 20; i++) {
+			new CozinhaThread(i).start();
+		}
+	}
+}
